c7d8e6f3dd947975516f98b89f450de80d0d6733
Mark Force <force.j.mark@gmail.com>
2021-02-10 08:53:52 -0500

    fixed deployment bug ci=false

--- src/Components/Hero/About.js
+++ src/Components/Hero/About.js
@@ -1,29 +1,39 @@
 import React, { useState } from 'react';
+import TextLoop from "react-text-loop";
 
 const About = () => {
 
-    const [textArr, setTextArr] = useState(['mobile-first development', 'innovation in web-design', 'accessibility-first development'])
-    const [currTextArr, setCurrTextArr] = useState('elegant development')    
+    // const [textArr, setTextArr] = useState(['mobile-first development', 'innovation in web-design', 'accessibility-first development'])
+    // const [currTextArr, setCurrTextArr] = useState('elegant development')    
     
 
-    let i = 0
-    const switchTextOverlay = () => {
-        setInterval(function() {
-            if(i > 3) i = 0
-            setCurrTextArr(textArr[i])
-            i++
-            console.log(textArr[i])
-        }, 8000)
-    }
+    // let i = 0
+    // const switchTextOverlay = () => {
+    //     setInterval(function() {
+    //         if(i > 3) i = 0
+    //         setCurrTextArr(textArr[i])
+    //         i++
+    //         console.log(textArr[i])
+    //     }, 8000)
+    // }
 
-    switchTextOverlay()
+    // switchTextOverlay()
 
     
     return (
         <>
             <div className='about' id='about'>
             <div className='hero-bg'></div>
-            <span className='bg-txt-overlay'>{currTextArr}</span>
+            <span className='bg-txt-overlay'>
+                <TextLoop
+                    interval={5000}
+                >
+                    <span>mobile-first development</span>
+                    <span>accessibility-first development</span>
+                    <span>innovation in web-design</span>
+                    <span>elegant development</span>
+                </TextLoop>
+            </span>
                 <div className='about-header'>
                     <h1>About</h1>
                 </div>
